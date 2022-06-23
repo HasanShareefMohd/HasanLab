c0f8a1281f6aa3ff5257192c02267dc75c905b3a
Hasan shareef mohd <107669781+HasanShareefMohd@users.noreply.github.com>
2022-06-23 14:38:25 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class User{
+public static void main(String[] args){
+Scanner Scn = new Scanner(System.in);
+System.out.println("Enter username");
+String userName = Scn.nextLine();
+System.out.println("User name:" +userName);
+}
+}
