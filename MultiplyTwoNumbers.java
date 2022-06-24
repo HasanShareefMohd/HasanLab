8181b0c4e2f50e1cab507aad87b8c2dc367c7d90
Hasan shareef mohd <107669781+HasanShareefMohd@users.noreply.github.com>
2022-06-24 11:00:45 +0530

    Update MultiplyTwoNumbers.java

--- MultiplyTwoNumbers.java
+++ MultiplyTwoNumbers.java
@@ -1,8 +1,10 @@
 class MultiplyTwoNumbers{
 public static void main(String[] args){
+  //here given the datatype and vraibles
 float one=10;
 float two=20;
 float three = one * two;
+  //here given the three variable for equal to one and two
 System.out.println("the three is:" + three);
 }
 }
