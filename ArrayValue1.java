7901f985f63284c6fc9302562b6403ba307baf51
Hasan shareef mohd <107669781+HasanShareefMohd@users.noreply.github.com>
2022-07-22 11:38:43 +0530

    Update ArrayValue1.java

--- ArrayValue1.java
+++ ArrayValue1.java
@@ -3,11 +3,11 @@ package Lab2;
 public class ArrayValue1 {
 	public static void main(String[] args) {
 		int[]  arr1 = {1, 2, 3, 4, 5};
-		boolean found = false;
+		boolean found = false; //return type method
 		int n = 5;
 		for(int x : arr1) {
-			if(x==n) {
-				found = true;
+			if(x==n) { //loop used for check elements
+				found = true; //if the array is equal to the given varaible it will print true
 				break;
 			}
 		}
